0d6d4743b4134d3e90e1e63b9d691878f29ce143
Bitnara <qlcskfk02@gmail.com>
2024-08-26 19:21:54 +0900

    문제행동 작성 완료화면 모달 적용

--- src/components/expanded/BehaviorCreateContents.vue
+++ src/components/expanded/BehaviorCreateContents.vue
@@ -17,7 +17,7 @@
               shape="square" 
               color="secondary" 
               placeholder="ex) 외출시 강아지 짖음이 너무 심해요" 
-              v-model="title" 
+              @changeInput="(data)=>{title = data}"
             />
           </div>
         </template>
@@ -29,7 +29,7 @@
             placeholder="강아지가 주로 행동하는 시간, 장소, 공간 등의 내용을 구체적으로 작성해보세요" 
             class="mb-3"
             size="lg"
-            v-model="content"
+            @changeInput="(data)=>{content = data}"
           />
         </template>
       </BehaviorCreateItem>
@@ -42,7 +42,7 @@
               shape="square" 
               color="secondary" 
               placeholder="ex) 1년 미만, 모름" 
-              v-model="duration" 
+              @changeInput="(data)=>{duration = data}"
             />
           </div>
         </template>
@@ -69,7 +69,7 @@
               shape="square" 
               color="secondary" 
               placeholder="ex) #성격 #상황" 
-              v-model="tag" 
+              @changeInput="(data)=>{tag = data}"
             />
           </div>
         </template>
@@ -77,34 +77,41 @@
     </div>
 
     <div class="button-container flex justify-center">
-      <Button
-        class="w45% mr-2"
-        color="quaternary"
-        @click="goBack"
-      >
-        뒤로
-      </Button>
-      <Button
-        class="w45%"
-        color="primary"
-        @click="saveAndComplete"
-      >
-        완료
-      </Button>
+      <Button class="w45% mr-2" color="quaternary" @click="goBack">뒤로</Button>
+      <Button class="w45%" color="primary" @click="openModalWithValidation">완료</Button>
     </div>
 
+    <Dialog
+      :isActive="isActive"
+      :translateY="translateY"
+      @close="closeModal"
+      @update:translateY="val => translateY = val"
+      style="z-index: 2000;"
+    >
+      <div class="p5 center-content" style="height: 250px;">
+        <p class="s-heading-00">작성한 글을 등록하시겠어요?</p>
+        <p class="text-color mt-3 mb-5">작성한 내용을 바탕으로 진단받을 수 있으며,<br>게시글이 올라간 후에도 수정할 수 있어요.</p>
+        <Button class="w-full mb-2" color="quaternary" @click="closeModal">닫기</Button>
+        <Button class="w-full" color="primary" @click="saveAndComplete">등록</Button>
+      </div>
+    </Dialog>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import { useRouter } from 'vue-router';
+
 import Button from "@/components/core/Button/Button.vue";
 import Card from "@/components/core/Card/Card.vue";
 import BehaviorCreateItem from "@/components/expanded/BehaviorCreateItem.vue";
 import Input from "@/components/core/Input/Input.vue";
 import InputTextarea from "@/components/core/Input/InputTextarea.vue";
 import Inputfile from "@/components/core/Input/Inputfile.vue";
-import { useRouter } from 'vue-router';
+import Dialog from "@/components/core/Dialog/Dialog.vue";
+
+import { useDialog } from "@/composables/useDialog";
+const { isActive, translateY, openModal, closeModal } = useDialog();
 
 const title = ref('');
 const content = ref('');
@@ -125,52 +132,36 @@ const handleFileUpload = (fileData) => {
 // 필수 입력 사항 검증
 const validateFields = () => {
   const fields = [
-    { value: title, error: titleError, message: '제목은 필수 입력 사항입니다.' },
-    { value: content, error: contentError, message: '세부 사항은 필수 입력 사항입니다.' },
-    { value: duration, error: durationError, message: '문제 행동 지속 기간은 필수 입력 사항입니다.' }
+    { value: title.value, error: titleError, message: '제목은 필수 입력 사항입니다.' },
+    { value: content.value, error: contentError, message: '세부 사항은 필수 입력 사항입니다.' },
+    { value: duration.value, error: durationError, message: '문제 행동 지속 기간은 필수 입력 사항입니다.' }
   ];
-
   let isValid = true;
-
   fields.forEach(field => {
-    if (!field.value.value) {
+    if (!field.value) {
       field.error.value = field.message;
       isValid = false;
     } else {
       field.error.value = '';
     }
   });
-
   return isValid;
 };
 
-const saveInfo = () => {
+const openModalWithValidation = () => {
   if (validateFields()) {
-    const problemInfo = {
-      title: title.value,
-      content: content.value,
-      duration: duration.value,
-      tag: tag.value,
-      image: imageData.value
-    };
-
-    localStorage.setItem('problemInfo', JSON.stringify(problemInfo));
-    return true;
+    openModal(); 
   }
-
-  return false;
 };
 
 const saveAndComplete = () => {
-  if (saveInfo()) {
-    alert('정보가 성공적으로 저장되었습니다.');
-    router.push({ path: '/' });
-  }
+  localStorage.removeItem('selectedCategories');
+  localStorage.removeItem('currentPage');
+  router.push({ path: '/' });
 };
 
-const emit = defineEmits(['back']);
 const goBack = () => {
-  emit('back');
+  router.back();
 };
 </script>
 
@@ -196,5 +187,13 @@ const goBack = () => {
   background-color: var(--s-semantic-primary-background-white-default);
   padding: 10px 0;
   z-index: 1000;
+}
+
+.center-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
 }
 </style>
